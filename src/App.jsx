9cb4d8ee692b1fc2c547f97967f99be5be3c3d36
Bryan Estrada <est.armando42@gmail.com>
2024-07-01 13:11:29 -0600

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import Counter from "./components/Counter"
+import UserProfile from "./components/UserProfile"
 
 const App = () => {
+
+  const users = [{
+    name: "José Maria",
+    age: 40,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  }, {
+    name: "Diluc",
+    age: 25,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  }, {
+    name: "Juan",
+    age: 20,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  }]
 
   return (
     <>
       <Counter />
+      {users.map((user, index) => (
+        <UserProfile key={index} user={user} />
+      ))}
     </>
   )
 }

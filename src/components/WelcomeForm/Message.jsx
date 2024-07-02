dacda84a5a1d2ae876141c00051399ef2f35824f
Bryan Estrada <est.armando42@gmail.com>
2024-07-02 16:51:48 -0600

    form-event-and-conditional-rendering

---
+++ src/components/WelcomeForm/Message.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Message.module.css";
+
+const Message = ({ user }) => {
+  if (!user.userName && !user.age) {
+    return null;
+  }
+
+  if (isNaN(user.age) || user.age == "") {
+    return (
+      <div>
+        <p className={styles.error}>The age value is incorrect, please try again!</p>
+      </div>
+    );
+  } else if (Number.parseInt(user.age) <= 18) {
+    return (
+      <div className={styles.warning}>
+        <p>{user.userName} you are not allowed!</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.valid}>
+        <p>Welcome {user.userName}</p>
+      </div>
+    );
+  }
+};
+
+export default Message;
---
+++ src/components/WelcomeForm/WelcomeForm.jsx
@@ -0,0 +1,37 @@
+import styles from "./WelcomeForm.module.css";
+import Message from "./Message";
+import { useState } from "react";
+
+const WelcomeForm = () => {
+  const [user, setUser] = useState({ userName: "", age: "" });
+
+  const handleSubmit = (e) => {
+    const { userName, age } = e.target;
+    setUser({
+      userName: userName.value,
+      age: age.value,
+    });
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Welcome Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userName">
+          Name:
+          <input type="text" name="userName" id="userName" />
+        </label>
+        <label htmlFor="age">
+          Age:
+          <input type="text" name="age" id="age" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+
+      <Message user={user} />
+    </div>
+  );
+};
+
+export default WelcomeForm;
